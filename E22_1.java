b3209aaac1dd2c40855c141270feba9c99289e0f
Md : Shibli Noman <139595891+muhammadshiblinoman@users.noreply.github.com>
2023-10-02 11:05:45 +0600

    Create E22_1.java

---
+++ E22_1.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+class Main {
+  public static void main(String[] args) {
+    LinkedList<String> languages = new LinkedList<>();
+    languages.add("Java");
+    languages.add("Python");
+    languages.add("JavaScript");
+    languages.add("Kotlin");
+    System.out.println("LinkedList: " + languages);
+    String str = languages.remove(3);
+    System.out.println("Removed Element: " + str);
+
+    System.out.println("Updated LinkedList: " + languages);
+  }
+}
